3c817fed3d220fd7e92293e58e8aa5bac6a9ca78
0w4n <jorgelevia@gmail.com>
2026-08-31 18:31:12 +0200

    Delete src out path

--- src/features/invitations/domain/invitation.mapper.ts
+++
@@ -1,8 +0,0 @@
-import {
-  FeatureMapper,
-  type Mapper,
-} from "#core/appCore/domain/AppCore.mapper";
-import type { CreatedInvitationDTO, Invitation } from "./invitation.entity";
-
-export const PanelMapper: Mapper<Invitation, CreatedInvitationDTO> =
-  FeatureMapper;
