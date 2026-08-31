71c3669111c2fe2a7784c1b7e3d8c2a084152976
0w4n <jorgelevia@gmail.com>
2026-08-31 18:56:36 +0200

    Hotfix

--- client/src/features/task/domain/task.mapper.ts
+++ client/src/features/task/domain/task.mapper.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD:client/src/features/task/domain/task.mapper.ts
 import { FeatureMapper, type Mapper } from "#core/appCore/domain/AppCore.mapper";
 import type { CreateAnyTaskDTO, Task } from "./task.entity";
 
-=======
-import { FeatureMapper, type Mapper } from "#core/appCore/domain/AppCore.mapper";
-import type { CreateAnyTaskDTO, Task } from "./task.entity";
-
->>>>>>> main/HEAD:src/features/task/domain/task.mapper.ts
 export const TaskMapper: Mapper<Task, CreateAnyTaskDTO> = FeatureMapper
