95fccaa971577822e674cbb0ae6fb2cfa4311ba6
0w4n <jorgelevia@gmail.com>
2026-09-25 13:38:18 +0200

    FIX: deploy pipe line for each enviroment

--- client/src/core/appCore/infraestructure/firebase/firebaseConfig.ts
+++ client/src/core/appCore/infraestructure/firebase/firebaseConfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
+import { getAnalytics, type Analytics} from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: import.meta.env.CLIENT_FIREBASE_API_KEY,
@@ -9,6 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.CLIENT_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.CLIENT_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.CLIENT_FIREBASE_APP_ID,
+  measurementId: import.meta.env.CLIENT_FIREBASE_MEASUREMENT_ID,
 };
 
 console.log(firebaseConfig);
@@ -17,11 +19,13 @@ class FirebaseService {
   private app: FirebaseApp;
   private _auth: Auth;
   private _firestore: Firestore;
+  private _analytics: Analytics;
 
   constructor() {
     this.app = initializeApp(firebaseConfig);
     this._auth = getAuth(this.app);
     this._firestore = getFirestore(this.app);
+    this._analytics = getAnalytics(this.app);
   }
 
   get auth(): Auth {
@@ -30,6 +34,10 @@ class FirebaseService {
 
   get firestore(): Firestore {
     return this._firestore;
+  }
+
+  get analytics(): Analytics {
+    return this._analytics;
   }
 }
 
